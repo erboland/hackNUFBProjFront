d7e969668909b6518a9bdd06dfa3abffa21eb703
erboland <karshigaerbol@gmail.com>
2021-03-21 03:40:00 +0600

    temporary api connection

--- src/Components/Registration.jsx
+++ src/Components/Registration.jsx
@@ -1,35 +1,66 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './signIn.css';
 import {Link, useHistory} from 'react-router-dom';
 
 function Registration() {
   const history = useHistory();
+
+  const [user, setUser] = useState ({email: "", password:"", firstName:"", lastName:"", login: ""});
+
+  const handleChange = (type, value) =>{
+    setUser({...user, type: value});
+  }
+
   const handleSubmit = () =>{
-    history.push('/room');
+    console.log(user);
+//    history.push('/room');
+//    fetch('http://62.171.186.222:9090/api/auth/login', {
+//      method: "POST",
+//      body: JSON.stringify(user),
+//      headers: {"Content-type": "application/json;charset=UTF-8", "Access-Control-Allow-Origin": "*"},
+//      mode:"no-cors"
+//    }).then(res=>{
+//      console.log(res);
+//    })
   }
 
   return (
     <div className="container">
-      <div className="signInContainer">
+      <div className="regContainer">
         <h1>Registration</h1>
-        <div className="inputRow">
-          <label for="id">
-            Your Full Name:
-          </label>
-          <input type="text" name="name"/>
+        <div className="inputs">
+          <div className="inputRow">
+            <label for="fName">
+              Your First Name:
+            </label>
+            <input type="text" name="fName" value={user.firstName} onChange={(e)=>handleChange("firstName", e.target.value)}/>
+          </div>
+          <div className="inputRow">
+            <label for="lName">
+              Your Last Name:
+            </label>
+            <input type="text" name="lName" value={user.lastName} onChange={(e)=>handleChange("lastName", e.target.value)}/>
+          </div>
+          <div className="inputRow">
+            <label for="email">
+              Your Email:
+            </label>
+            <input type="email" name="email" value={user.email} onChange={(e)=>handleChange("email", e.target.value)} />
+          </div>
+          <div className="inputRow">
+            <label for="password">
+              Your Password?
+            </label>
+            <input name="password" type="password" value={user.password} onChange={(e)=>handleChange("password", e.target.value)}/>
+          </div>
+          <div className="inputRow">
+            <label for="login">
+              Your Login:
+            </label>
+            <input name="text" type="login" value={user.login} onChange={(e)=>handleChange("login", e.target.value)}/>
+          </div>
         </div>
-        <div className="inputRow">
-          <label for="password">
-            Your Password?
-          </label>
-          <input name="password" type="password"/>
-        </div>
-        <div className="inputRow">
-          <label for="email">
-            Your Email:
-          </label>
-          <input name="email" type="email"/>
-        </div>
+
         <button className="loginButton" onClick={handleSubmit}>Submit</button>
         <Link className="noRegister" to="/">Go back</Link>
       </div>
--- src/Components/SignIn.jsx
+++ src/Components/SignIn.jsx
@@ -1,12 +1,30 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './signIn.css';
 import {Link, useHistory} from 'react-router-dom';
 
+
 function SignIn() {
   const history = useHistory();
+  const [user, setUser] = useState ({email: "", password:"", rememberMe: false})
 
   const handleClick = () =>{
-    history.push('/room');
+//    history.push('/room');
+    fetch('http://62.171.186.222:9090/api/auth/login', {
+      method: "POST",
+      body: JSON.stringify(user),
+      headers: {"Content-type": "application/json;charset=UTF-8", "Access-Control-Allow-Origin": "*"},
+    }).then(res=>{
+      console.log(res);
+    })
+  }
+
+  const changeUser = (type, value) =>{
+
+    if (type === "password") {
+      setUser({...user, password: value});
+    } else {
+      setUser({...user, email: value});
+    }
   }
 
   return (
@@ -15,15 +33,15 @@ function SignIn() {
         <h1>Hello!</h1>
         <div className="inputRow">
           <label for="id">
-            Your ID:
+            Your Email:
           </label>
-          <input type="text" name="id"/>
+          <input type="text" name="id" value={user.email} onChange={(e)=>changeUser("email", e.target.value)}/>
         </div>
         <div className="inputRow">
           <label for="password">
             Your Password?
           </label>
-          <input name="password" type="password"/>
+          <input name="password" type="password" value={user.password} onChange={(e)=>changeUser("password", e.target.value)}/>
         </div>
         <button className="loginButton" onClick={handleClick}>Enter</button>
         <Link className="noRegister" to="/registration">Have not already registered?</Link>
